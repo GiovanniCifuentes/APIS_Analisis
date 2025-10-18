df9505faaf543348e586632190f94f70183b3a37
unknown <giovannicifuentescss20@gmail.com>
2025-10-18 10:56:33 -0600

    Limpieza de archivos temporales y configuración .gitignore

--- CRMVentasAPI/Cliente.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CRMVentasAPI.Models
-{
-    public class Cliente
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Email { get; set; }
-    }
-}
